9b811d1cd73fe1a25f75098de16ddd7c3eeea8fe
sujapc1 <sujaypc1@gmail.com>
2019-01-29 16:57:06 +0530

    api and Queriable States with Schema

--- clients/src/main/kotlin/com/template/webserver/Controller.kt
+++ clients/src/main/kotlin/com/template/webserver/Controller.kt
@@ -1,9 +1,49 @@
 package com.template.webserver
 
+
+
+import com.template.flows.InitiateLoanFlow
+import com.template.states.LoanRequestState
+import net.corda.core.identity.Party
+/*
+import com.template.flows.InitiateLoanFlow
+import com.template.flows.LoanResponseFlow
+import com.template.contracts.LoanRequestContract
+import com.template.contracts.LoanVerificationContract
+import com.template.states.LoanVerificationState
+import com.template.Schema.LoanRequestSchemaV1
+import com.template.Schema.LoanVerificationSchemaV1
+*/
+
+import net.corda.core.messaging.CordaRPCOps
+import net.corda.core.utilities.OpaqueBytes
+import net.corda.core.utilities.getOrThrow
+import java.util.*
+/*import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+import javax.ws.rs.core.Response.Status.BAD_REQUEST
+import javax.ws.rs.core.Response.Status.CREATED*/
+
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+
+
+/*import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces*/
+
+/*import javax.enterprise.inject.Produces
+import javax.ws.rs.GET
+import javax.ws.rs.Path*/
 
 /**
  * Define your API endpoints here.
@@ -13,7 +53,7 @@ import org.springframework.web.bind.annotation.RestController
 class Controller(rpc: NodeRPCConnection) {
 
     companion object {
-        private val logger = LoggerFactory.getLogger(RestController::class.java)
+        private val logger = LoggerFactory.getLogger(Controller::class.java)
     }
 
     private val proxy = rpc.proxy
@@ -22,4 +62,69 @@ class Controller(rpc: NodeRPCConnection) {
     private fun templateendpoint(): String {
         return "Define an endpoint here."
     }
+
+    private val myIdentity = proxy.nodeInfo().legalIdentities.first()
+
+    // Node info
+    @GetMapping(value = "/me", produces = arrayOf("text/plain"))
+    fun me():String
+    {
+        return(myIdentity.name.toString())
+    }
+
+     //Peer info
+    @GetMapping(value = "/peers", produces = arrayOf("text/plain"))
+    fun peers(): String {
+        return mapOf("peers" to proxy.networkMapSnapshot()
+                .filter { nodeInfo -> nodeInfo.legalIdentities.first() != myIdentity }
+                .map { it.legalIdentities.first().name.organisation }).toString()
+    }
+
+
+
+    //LonRequestState
+    @GetMapping(value = "/LoanRequestState", produces = arrayOf("text/plain"))
+    fun LoanRequestState(): String {
+        return (proxy.vaultQuery(LoanRequestState::class.java).states).toString()
+    }
+
+    //InitiateLoanFLow
+    @GetMapping(value = "/InitiateLoanFlow", produces = arrayOf("text/plain"))
+    fun InitiateLoanFlow(
+            @RequestParam("LoanAmount") LoanAmount: Int,
+            @RequestParam("CustomerName") CustomerName: String,
+            @RequestParam("party") party : String
+            ): String {
+
+        // 1. Get party objects for the counterparty.
+        val Bank = proxy.partiesFromName(party, exactMatch = false).singleOrNull()
+                ?: throw IllegalStateException("Couldn't lookup node identity")
+
+
+        // 2. Start the InitiateLoanFlow. We block and wait for the flow to return.
+
+        val (status) = try {
+            val flowHandle = proxy.startFlowDynamic(
+                    InitiateLoanFlow.Initiator::class.java,
+                    Bank,
+                    CustomerName,
+                    LoanAmount
+            )
+
+            flowHandle.use { flowHandle.returnValue.getOrThrow() }
+            HttpStatus.CREATED to "Initiated loan"
+        } catch (e: Exception) {
+            HttpStatus.BAD_REQUEST to e.message
+        }
+
+        // return HttpStatus.status(status).entity(message).build()
+        return status.toString()
+    }
+
+
+*//*@GET
+@Path("LoanAmount")
+@Produces(MediaType.APPLICATION_JSON)
+fun LoanAmount() = proxy.vaultQuery(LoanRequestState::class.java).LoanAmount*//*
+*/
 }
--- cordapp/src/main/kotlin/com/template/states/LoanRequestState.kt
+++ cordapp/src/main/kotlin/com/template/states/LoanRequestState.kt
@@ -1,9 +1,13 @@
 package com.template.states
 
-import net.corda.core.contracts.ContractState
+import com.template.Schema.LoanRequestSchemaV1
 import net.corda.core.contracts.LinearState
 import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.AbstractParty
 import net.corda.core.identity.Party
+import net.corda.core.schemas.MappedSchema
+import net.corda.core.schemas.PersistentState
+import net.corda.core.schemas.QueryableState
 
 /**
  * The state object recording LoanRequestState between Finance Agecy and Bank.
@@ -22,10 +26,27 @@ data class LoanRequestState(val LoanAmount: Int,
                             val Bank: Party,
                             val FinanceAgency: Party,
                             val isEligibleForLoan: Boolean = false,
-                    override val linearId: UniqueIdentifier = UniqueIdentifier()): LinearState {
+                    override val linearId: UniqueIdentifier = UniqueIdentifier()): LinearState, QueryableState {
+    //constructor() : this()
 
     /** The public keys of the involved parties. */
     override val participants get() = listOf(Bank, FinanceAgency)
+
+    override fun generateMappedObject(schema: MappedSchema): PersistentState {
+        return when (schema) {
+            is LoanRequestSchemaV1 -> LoanRequestSchemaV1.PersistentIOU(
+                    this.LoanAmount,
+                    this.CustomerName,
+                    this.Bank.name.toString(),
+                    this.FinanceAgency.name.toString(),
+                    this.isEligibleForLoan,
+                    this.linearId.id
+            )
+            else -> throw IllegalArgumentException("Unrecognised schema $schema")
+        }
+    }
+
+    override fun supportedSchemas(): Iterable<MappedSchema> = listOf(LoanRequestSchemaV1)
 
 }
 
