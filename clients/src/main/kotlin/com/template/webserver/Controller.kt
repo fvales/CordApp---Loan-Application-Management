ecc557effd6d2bbe7497a55d5559431707323595
sujapc1 <sujaypc1@gmail.com>
2019-01-31 16:57:54 +0530

    API's

--- clients/src/main/kotlin/com/template/webserver/Controller.kt
+++ clients/src/main/kotlin/com/template/webserver/Controller.kt
@@ -3,8 +3,37 @@ package com.template.webserver
 
 
 import com.template.flows.InitiateLoanFlow
+import com.template.flows.LoanResponseFlow
+import com.template.flows.RequestCRAFlow
 import com.template.states.LoanRequestState
+import com.template.flows.CRAResponse
+import com.template.states.LoanVerificationState
+import net.corda.core.contracts.UniqueIdentifier
+
+import net.corda.core.identity.Party
+/*
+import com.template.flows.InitiateLoanFlow
+import com.template.flows.LoanResponseFlow
+import com.template.contracts.LoanRequestContract
+import com.template.contracts.LoanVerificationContract
+import com.template.states.LoanVerificationState
+import com.template.Schema.LoanRequestSchemaV1
+import com.template.Schema.LoanVerificationSchemaV1
+*/
+
+import net.corda.core.messaging.CordaRPCOps
+import net.corda.core.utilities.OpaqueBytes
 import net.corda.core.utilities.getOrThrow
+import java.util.*
+/*import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.QueryParam
+import javax.ws.rs.core.MediaType
+import javax.ws.rs.core.Response
+import javax.ws.rs.core.Response.Status.BAD_REQUEST
+import javax.ws.rs.core.Response.Status.CREATED*/
+
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -12,8 +41,16 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID.fromString
 
 
+/*import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces*/
+
+/*import javax.enterprise.inject.Produces
+import javax.ws.rs.GET
+import javax.ws.rs.Path*/
 
 /**
  * Define your API endpoints here.
@@ -37,8 +74,9 @@ class Controller(rpc: NodeRPCConnection) {
 
     // Node info
     @GetMapping(value = "/me", produces = arrayOf("text/plain"))
-    fun me(): String {
-        return (myIdentity.name.toString())
+    fun me():String
+    {
+        return(myIdentity.name.toString())
     }
 
     //Peer info
@@ -50,10 +88,17 @@ class Controller(rpc: NodeRPCConnection) {
     }
 
 
+
     //LonRequestState
     @GetMapping(value = "/LoanRequestState", produces = arrayOf("text/plain"))
     fun LoanRequestState(): String {
         return (proxy.vaultQuery(LoanRequestState::class.java).states).toString()
+    }
+
+    //LonVerificationState
+    @GetMapping(value = "/LonVerificationState", produces = arrayOf("text/plain"))
+    fun LoanVerificationState(): String {
+        return (proxy.vaultQuery(LoanVerificationState::class.java).states).toString()
     }
 
     //InitiateLoanFLow
@@ -61,7 +106,7 @@ class Controller(rpc: NodeRPCConnection) {
     fun InitiateLoanFlow(
             @RequestParam("LoanAmount") LoanAmount: Int,
             @RequestParam("CustomerName") CustomerName: String,
-            @RequestParam("party") party: String
+            @RequestParam("party") party : String
     ): String {
 
         // 1. Get party objects for the counterparty.
@@ -69,7 +114,7 @@ class Controller(rpc: NodeRPCConnection) {
                 ?: throw IllegalStateException("Couldn't lookup node identity")
 
 
-        // 2. Start the InitiateLoanFlow. We block and wait for the flow to return.
+        // 2. Start the InitiateLoan Flow. We block and wait for the flow to return.
 
         val (status) = try {
             val flowHandle = proxy.startFlowDynamic(
@@ -89,9 +134,100 @@ class Controller(rpc: NodeRPCConnection) {
         return status.toString()
     }
 
-}
-//@GET
-//@Path("LoanAmount")
-//@Produces(MediaType.APPLICATION_JSON)
-//fun LoanAmount() = proxy.vaultQuery(LoanRequestState::class.java).LoanAmount*//*
+    //RequestCRAFLow
+    @GetMapping(value = "/RequestCRAFLow", produces = arrayOf("text/plain"))
+    fun RequestCRAFLow(
+            @RequestParam("linearIdentifier") linearIdentifier: String,
+            @RequestParam("party") party : String
+    ): String {
 
+        // 1. Get party objects for the counterparty.
+        val CreditRatingAgency = proxy.partiesFromName(party, exactMatch = false).singleOrNull()
+                ?: throw IllegalStateException("Couldn't lookup node identity")
+
+
+        // 2. Start the RequestCRA FLow. We block and wait for the flow to return.
+
+        val (status) = try {
+            val flowHandle = proxy.startFlowDynamic(
+                    RequestCRAFlow.Initiator::class.java,
+                    CreditRatingAgency,
+                    UniqueIdentifier.fromString(linearIdentifier)
+            )
+
+            flowHandle.use { flowHandle.returnValue.getOrThrow() }
+            HttpStatus.CREATED to "Request CRA"
+        } catch (e: Exception) {
+            HttpStatus.BAD_REQUEST to e.message
+        }
+
+        // return HttpStatus.status(status).entity(message).build()
+        return status.toString()
+    }
+
+    //CRAResponse
+    @GetMapping(value = "/CRAResponse", produces = arrayOf("text/plain"))
+    fun CRAResponse(
+            @RequestParam("linearIdentifier") linearIdentifier: String,
+            @RequestParam("party") party : String
+    ): String {
+
+        // 1. Get party objects for the counterparty.
+        val Bank = proxy.partiesFromName(party, exactMatch = false).singleOrNull()
+                ?: throw IllegalStateException("Couldn't lookup node identity")
+
+
+        // 2. Start the CRAResponse FLow. We block and wait for the flow to return.
+
+        val (status) = try {
+            val flowHandle = proxy.startFlowDynamic(
+                    CRAResponse.Initiator::class.java,
+                    Bank,
+                    UniqueIdentifier.fromString(linearIdentifier)
+            )
+
+
+
+            flowHandle.use { flowHandle.returnValue.getOrThrow() }
+            HttpStatus.CREATED to "CRA Response"
+        } catch (e: Exception) {
+            HttpStatus.BAD_REQUEST to e.message
+        }
+
+        // return HttpStatus.status(status).entity(message).build()
+        return status.toString()
+    }
+
+    //LoanResponseFLow
+    @GetMapping(value = "/LoanResponseFLow", produces = arrayOf("text/plain"))
+    fun LoanResponseFLow(
+            @RequestParam("linearIdentifier") linearIdentifier: String,
+            @RequestParam("party") party : String
+    ): String {
+
+        // 1. Get party objects for the counterparty.
+        val FinanceAgency = proxy.partiesFromName(party, exactMatch = false).singleOrNull()
+                ?: throw IllegalStateException("Couldn't lookup node identity")
+
+
+        // 2. Start the LoanResponseFLow. We block and wait for the flow to return.
+
+        val (status) = try {
+            val flowHandle = proxy.startFlowDynamic(
+                    LoanResponseFlow.Initiator::class.java,
+                    FinanceAgency,
+                    UniqueIdentifier.fromString(linearIdentifier)
+            )
+
+            flowHandle.use { flowHandle.returnValue.getOrThrow() }
+            HttpStatus.CREATED to "Loan Response"
+        } catch (e: Exception) {
+            HttpStatus.BAD_REQUEST to e.message
+        }
+
+        // return HttpStatus.status(status).entity(message).build()
+        return status.toString()
+    }
+
+
+}
